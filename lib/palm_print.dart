09f49bd9ba6603c9e23e2371d33814817961f26d
Charles Gao <1030587461@qq.com>
2022-05-23 19:54:53 +0800

    build framework for palm-print mobile end

---
+++ lib/palm_print.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const PalmPrint());
+}
+class PalmPrint extends StatelessWidget {
+  const PalmPrint({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
