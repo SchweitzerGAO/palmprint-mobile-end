f16adcef2502b4eccfccf2b93519a599d2ab55db
Charles Gao <1030587461@qq.com>
2022-05-23 11:28:42 +0800

    finished the tutorial by changing the theme.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       theme: ThemeData(
-        primaryColor: Colors.red,
+        primarySwatch: Colors.pink
       ),
       home: const RandomWords(),
     );
