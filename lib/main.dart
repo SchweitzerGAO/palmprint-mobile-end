7ec54c92ad79ce447307c92e08eda26f9d5dc3ee
Charles Gao <1030587461@qq.com>
2022-05-23 19:52:56 +0800

    fixed bugs for theme changing

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,29 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) {
+    const MaterialColor white = MaterialColor(
+      0xFFFFFFFF,
+      <int, Color>{
+        50: Color(0xFFFFFFFF),
+        100: Color(0xFFFFFFFF),
+        200: Color(0xFFFFFFFF),
+        300: Color(0xFFFFFFFF),
+        400: Color(0xFFFFFFFF),
+        500: Color(0xFFFFFFFF),
+        600: Color(0xFFFFFFFF),
+        700: Color(0xFFFFFFFF),
+        800: Color(0xFFFFFFFF),
+        900: Color(0xFFFFFFFF),
+      },
+    );
     return MaterialApp(
       title: 'Startup Name Generator',
       theme: ThemeData(
-        primarySwatch: Colors.pink
+        primarySwatch: white
       ),
       home: const RandomWords(),
     );
